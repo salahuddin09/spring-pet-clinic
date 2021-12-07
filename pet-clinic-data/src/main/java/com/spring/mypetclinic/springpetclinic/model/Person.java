e6a0365caf54c34c1b57097d57d231160db5c32f
salahuddin09 <salahuddincse09@gmail.com>
2021-12-07 22:20:16 +0600

    Adding Base Entity.Closes #9

--- pet-clinic-data/src/main/java/com/spring/mypetclinic/springpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/spring/mypetclinic/springpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.spring.mypetclinic.springpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 
